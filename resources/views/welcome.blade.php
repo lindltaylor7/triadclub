a65fc845b8c7bffa0423802b0c904e6aabbee808
lindltaylor7 <riuyagami@gmail.com>
2021-04-23 17:40:52 -0500

    Avance 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,13 @@
     @livewire('carousel-main')
 
     @livewire('buscador')
-{{--
+
     @livewire('section-rubros')
 
     @livewire('section-rubros')
 
     <h1 class="text-white text-3xl ml-14">Empresas Top</h1>
 
-    @livewire('carousel-secondary') --}}
+    @livewire('carousel-secondary')
 
 </x-app-layout>

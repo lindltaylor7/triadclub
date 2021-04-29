16b28fa6ca0c382a7ed972dfdc6632fc87e9a007
lindltaylor7 <riuyagami@gmail.com>
2021-04-28 20:04:39 -0500

    Buscador

--- resources/views/livewire/buscador.blade.php
+++ resources/views/livewire/buscador.blade.php
@@ -5,19 +5,19 @@
 
 
         @if ($empresas->first())
-        <div class="flex flex-row mt-3 justify-center">
+        <div class="flex flex-row mt-3 mb-5 justify-center">
             @foreach ($empresas as $empresa)
                 @if ($empresa->logo=="red")
                 <div class="absolute">
                     <img src="{{Storage::url('empresas/coming.jfif')}}" class="px-4" width="320" height="180" alt="Logo">
-                    <p class="relative">{{$empresa->name}}</p>
+                    <a href="{{route('landing',$empresa->id)}}">{{$empresa->name}}</a>
                 </div>
 
 
                     @else
                     <div class="">
                         <img src="{{Storage::url($empresa->logo)}}" class="res-logo" width="320" height="180px" alt="Logo">
-                        <p class="text-center">{{$empresa->name}}</p>
+                        <a href="{{route('landing',1)}}" class="cursor-pointer">{{$empresa->name}}</a>
                     </div>
 
                 @endif
